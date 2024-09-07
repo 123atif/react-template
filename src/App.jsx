039806a1d3bc63b5047ae5a9e1fdcd786047e1b8
Atif <atifarshad0315@gmail.com>
2024-09-07 18:55:23 +0500

    add images / icons in assests and setup tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./index.css";
-
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="App">
+        <header className="bg-blue-500 text-white p-4">
+          <h1 className="text-xl">Welcome to React with Tailwind CSS!</h1>
+        </header>
+      </div>
+    </>
+  );
 }
 
 export default App;
